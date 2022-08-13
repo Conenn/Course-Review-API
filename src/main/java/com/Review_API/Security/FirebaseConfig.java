56c9c6d598e064e6aca92f33b82040045809adfa
Conenn <sda1364@wgu.edu>
2022-08-12 21:29:31 -0500

    Firebase Security

--- src/main/java/com/Review_API/Security/FirebaseConfig.java
+++ src/main/java/com/Review_API/Security/FirebaseConfig.java
@@ -1,46 +1,43 @@
 package com.Review_API.Security;
 
-import com.Review_API.Properties.SecurityProperties;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.ClassPathResource;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Autowired
-    SecurityProperties secProps;
+    private final String firebaseCredentials;
+
+    public FirebaseConfig(@Value("${SERVICE_ACCOUNT}") String firebaseCredentials) {
+        this.firebaseCredentials = firebaseCredentials;
+    }
 
     @Primary
     @Bean
-    public void firebaseInit() {
-        InputStream inputStream = null;
-        try {
-            inputStream = new ClassPathResource("firebase_config.json").getInputStream();
-        } catch (IOException e3) {
-            e3.printStackTrace();
+    public FirebaseApp firebaseApp() throws IOException {
+        if (FirebaseApp.getApps().isEmpty()) {
+            InputStream credentialsStream = new ByteArrayInputStream(firebaseCredentials.getBytes());
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(credentialsStream)).build();
+            FirebaseApp.initializeApp(options);
         }
-        try {
+        return FirebaseApp.getInstance();
+    }
 
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(inputStream))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-            System.out.println("Firebase Initialize");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+        return FirebaseAuth.getInstance(firebaseApp());
     }
 
 }
